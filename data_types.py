c4090abf3356d3d8f827287e34ae36215725bb0c
guna7222 <rrgunasekhar@gmail.com>
2021-03-26 19:31:59 +0530

    updated all the data types with examples

--- data_types.py
+++ data_types.py
@@ -1,5 +1,4 @@
 # Data types in python
-
 '''
 1)Text data type :-  str,
 2)numbers:- int, float, complex
@@ -45,3 +44,59 @@ num1 = -25.5 #Negetive float
 print(num1)
 print(type(num1))
 
+# sequence data types 
+
+fruits = ['apple', 'banana', 52] # list
+print(fruits)
+print(type(fruits))
+
+# tuple
+comp_languages = ('python', 'go', 'sql')
+print(comp_languages)
+print(type(comp_languages))
+
+# mapping data type
+# dictionary
+age = {'guna':22, 'thrilok':22}
+print(age)
+print(type(age))
+
+# set data type
+# set
+
+sports = {'cricket', 'shuttle', 'football', 'cricket', 'shuttle'}
+print(sports)
+print(type(sports))
+
+# frozenset 
+
+sports = frozenset({'cricket', 'shuttle', 'football', 'cricket', 'shuttle'})
+print(sports)
+print(type(sports))
+
+# bool
+
+'''
+will return True or False 
+'''
+val = True
+print(val)
+print(type(val))
+
+#binary data types 
+# bytes
+
+x = bytes("hello")
+print(x)
+print(type(x))
+
+#bytearray
+y = bytearray(73)
+print(y)
+print(type(y))
+
+#memoryview
+
+z = memoryview(bytes(100))
+print(z)
+print(type(z))
