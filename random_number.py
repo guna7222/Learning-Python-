9f343855e17b3b38cc6743a0a589a5f668ba1796
guna7222 <rrgunasekhar@gmail.com>
2021-03-26 19:57:51 +0530

    added random number program

---
+++ random_number.py
@@ -0,0 +1,9 @@
+# random number
+'''
+python does not have a random function to print random numbers,
+but python has ain-built module called random to print random numbers
+'''
+import random # in-built module
+x = random.randint(1, 100)
+print(type(x))
+print(x)
