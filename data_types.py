c5db50f74fb5e5674d11d4455cb6bd8da7edf954
guna7222 <rrgunasekhar@gmail.com>
2021-03-26 10:48:29 +0530

    added string and float data-types

---
+++ data_types.py
@@ -0,0 +1,47 @@
+# Data types in python
+
+'''
+1)Text data type :-  str,
+2)numbers:- int, float, complex
+3)sequence data type:- list,tuple,range
+4)mapping data type:- dictionary
+5)set data type:- set, frozenset
+6)bool = bool
+7)binary data type:- bytes, bytearrya, memoryview
+'''
+
+'''
+A string is a sequence of characters and surronded with either single 
+quotes(' ') are double quotes(" ").
+
+'''
+a = 'hello world' #text data type
+print(a)
+print(type(a))
+
+# Numbers data types
+'''
+integer (int or integer is a whole number, positive number or negative number
+without decimal points with length of unlimited. 
+'''
+b = 10 # positive integer
+print(b)
+print(type(b))
+
+c = -29 #Negetive integer
+print(c)
+print(type(c))
+
+# float or floating point 
+'''
+float or floating point is number, positive, negative with atleast one or more
+decimal points 
+'''
+num = 25.23 # positive integer
+print(num)
+print(type(num))
+
+num1 = -25.5 #Negetive float
+print(num1)
+print(type(num1))
+
