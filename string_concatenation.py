ede52fa88fd4a61d3b76ca6a90c519beec08219c
guna7222 <rrgunasekhar@gmail.com>
2021-03-27 12:17:38 +0530

    added string concatenation

---
+++ string_concatenation.py
@@ -0,0 +1,21 @@
+# String concatenation
+a = "AWS"
+b = " Stands for Amazon Web Service"
+combine = a+b # to combine or concatenate two strings we can use (+) operator
+print(combine)
+
+# To add a space between them 
+# string concatenation
+a = "AWS"
+b = "Stands for Amazon Web Service"
+combine =  a+ " " +b 
+print(combine)
+
+# string concatenation
+a = "AWS"
+b = " Stands for Amazon Web Service"
+c = "Linux"
+combine = a+b+" "+c # combining more than 2 strings 
+print(combine)
+
+
