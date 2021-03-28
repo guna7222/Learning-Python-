54dc57fee33eab0030c0bcabe60610bd47788907
guna7222 <rrgunasekhar@gmail.com>
2021-03-28 21:26:12 +0530

    added some string methods

---
+++ string_methods2.py
@@ -0,0 +1,18 @@
+# string methods
+# .title()
+# converts first character of each word into uppercase
+
+txt = "welcome to my world"
+print(txt.title())
+
+# .isupper returns True or False
+
+txt = "welcome to my world"
+print(txt.isupper())
+
+# .islower also returns True or False
+
+txt = "welcome to my world"
+print(txt.islower())
+
+
