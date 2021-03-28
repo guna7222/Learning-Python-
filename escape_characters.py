32f7342b5c96f8d21856455257b4ab60477b422c
guna7222 <rrgunasekhar@gmail.com>
2021-03-28 06:58:21 +0530

    Escape character's in strings

---
+++ escape_characters.py
@@ -0,0 +1,20 @@
+# Escape character's
+# example
+a = 'Hi it\'s about learning and exploring'
+
+b = "Hi it's about \"earning\" python & exploring"
+
+c = "hi print this in \nnew line" #\n prints in new line
+
+d = "hi print after a \ttab space"
+
+e = "hi print this in a \rnew line"
+
+f = "hi give \bback space and print"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
