c976be6758dbd1577f25cd78977a09a4b633abf5
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-29 09:05:37 +0530

    Update string_methods2.py

--- string_methods2.py
+++ string_methods2.py
@@ -1,18 +1,30 @@
 # string methods
 # .title()
-# converts first character of each word into uppercase
-
+# converts first character of each word into uppercase.
 txt = "welcome to my world"
 print(txt.title())
 
-# .isupper returns True or False
-
+# .isupper returns True or False.
 txt = "welcome to my world"
 print(txt.isupper())
 
-# .islower also returns True or False
-
+# .islower also returns True or False.
 txt = "welcome to my world"
 print(txt.islower())
 
+#isalpha (Returns True if all the character's in the string are alphabets(a-z).
+txt = "welcome to my world"
+print(txt.isalpha())
+
+#isnumeric returns True if all the character's in the string numbers.
+txt = "154615"
+print(txt.isnumeric())
+
+#endswith return's True if the specified value present at the ending of the string.
+txt = "so, this is the example about endswith method."
+print(txt.endswith('.'))
+
+#startswith returns True if the specified value present at the begining of the string.
+txt = "so, this is the example about endswith method."
+print(txt.startswith('s'))
 
