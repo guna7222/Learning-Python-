c1ed1a524d510eb43c3555ce4b3d59035b0e8af5
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-30 10:18:30 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,21 @@
+# Python Operators
+'''
+1)Arithmetic Operators
+2)Assignment Operators
+3)Comparision Operators
+4)Logical Operators
+5)Identity Operators
+6)Membership Operators
+7)Bitwise Operators
+'''
+'''
+1)Arithmetic operators
++ Addition 
+- Subtraction 
+* Multiplication 
+/ Division
+% modulus
+** Exponentiation 
+// Floor Division 
+
+# Example for Addition 
