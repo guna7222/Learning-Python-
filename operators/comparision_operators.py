8759c1543701819407ac10580939c5408cb924af
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 15:31:58 +0530

    Create comparision_operators.py

---
+++ operators/comparision_operators.py
@@ -0,0 +1,48 @@
+# Comparision operators
+x = 10
+y = 10
+if(x == y): # equal to(==) operator checks both number's are equal or not 
+	print("yes, both are equal")
+else:
+	print("no, both are not equal")
+
+
+#Not equal to(!=)
+a = 20
+b = 40
+if  a != b:
+	print("yes")
+else:
+	print("no")
+
+#Greater than or not (>)
+a = 20
+b = 40
+if  a>b:
+	print("yes")
+else:
+	print("no")
+
+#Less than (<)
+a = 20
+b = 40
+if  a<b:
+	print("yes")
+else:
+	print("no")
+
+#Greater than or equal to(>=)
+a = 20
+b = 40
+if  a>=b:
+	print("yes")
+else:
+	print("no")
+
+#Less than or equal to(<=)
+a = 20
+b = 40
+if  a<=b:
+	print("yes")
+else:
+	print("no")
