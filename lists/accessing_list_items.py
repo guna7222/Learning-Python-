3cdcddd14ac2978c2b7ccfc03604ecbf4a9cf3cc
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 19:20:17 +0530

    Update accessing_list_items.py

--- lists/accessing_list_items.py
+++ lists/accessing_list_items.py
@@ -7,3 +7,18 @@ print(friends[0]) # the first item has a index [0]
 friends = ["guna", "Thrilok", "Hemanth", "Chandu"]
 print(friends)
 print(friends[-2]) 
+
+#range 
+friends = ["guna", "Thrilok", "Hemanth", "Chandu"]
+print(friends[1:])
+
+# check if item exists
+friends = ["guna", "Thrilok", "Hemanth", "Chandu"]
+if "guna" in friends:
+  print("yeah! he is avaliable")
+
+# range of negative indexes 
+friends = ["guna", "Thrilok", "Hemanth", "Chandu"]
+print(friends[-3:-1]) # -1 is excluded 
+
+
