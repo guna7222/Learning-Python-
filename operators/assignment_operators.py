86ef93acc769527b0dc234dc43f0d7d502150b27
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 11:47:44 +0530

    Update assignment_operators.py

--- operators/assignment_operators.py
+++ operators/assignment_operators.py
@@ -1,2 +1,31 @@
 # 2)Assignment Operators
-x = 20
+x = 2
+print(x)
+
+x = 2
+x = x+3 # x=x+3 is same as  x+=3
+print(x)
+
+x = 3
+x = x-1 # x=x-1 is same as x-=1
+print(x)
+
+x = 3
+x = x*3 # x=x*3 is same as x*=3
+print(x)
+
+x = 10
+x = x/3 # x=x/3 is same as x/=3
+print(x)
+
+x = 10
+x = x%2 # x=x%2 is same as x%=2
+print(x)
+
+x = 10
+x = x//2 # x=x//2 is same as x//=3
+print(x)
+
+x = 10
+x = x**5 # x=x**2 is also same as x**=2
+print(x)
