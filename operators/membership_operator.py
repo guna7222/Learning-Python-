ae544a0fb370391f5615a5421f3cb84b0a917637
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 18:03:31 +0530

    Update membership_operator.py

--- operators/membership_operator.py
+++ operators/membership_operator.py
@@ -5,3 +5,11 @@ are not
 '''
 x = ["Docker", "Azure", "Linux"]
 print("Linux" in x)
+
+'''
+not in membership operator returns True if the specified value is not present in
+the object 
+'''
+x = ["Docker", "Azure", "Linux"]
+print("Linux" not in x) # False 
+
