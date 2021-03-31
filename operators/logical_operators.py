e043de15ed52a0994d2a25e50cb03c8829ca7d5e
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 15:53:44 +0530

    Update logical_operators.py

--- operators/logical_operators.py
+++ operators/logical_operators.py
@@ -8,7 +8,19 @@
 val = 10
 val1 = 20
 val2 = 30
-if (val < val1) and (val2 > val1):
+if (val < val1) and (val2 > val1): # both statements are True 
   print("Both statements are True")
 else:
   print("Both statements are False")
+  
+  
+# or
+val = 10
+val1 = 20
+val2 = 30
+if (val > val1) and (val2 > val1): # here one statement is False and other one is True 
+  print("Both statements are True")
+else:
+  print("Both statements are False")
+  
+  
