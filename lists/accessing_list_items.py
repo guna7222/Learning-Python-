dcdfcfc67b727771afc8acb75e1a1aeae48b99d9
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 19:12:25 +0530

    Create accessing_list_items.py

---
+++ lists/accessing_list_items.py
@@ -0,0 +1,9 @@
+#accessing list items 
+friends = ["guna", "Thrilok", "Hemanth", "Chandu"]
+print(friends)
+print(friends[0]) # the first item has a index [0]
+
+# Negative indexing starts from -1
+friends = ["guna", "Thrilok", "Hemanth", "Chandu"]
+print(friends)
+print(friends[-2]) 
