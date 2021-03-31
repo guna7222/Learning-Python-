1121abfd586610875263ac2ef953c519ac10f2d9
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 18:54:38 +0530

    Create creating_list.py

---
+++ lists/creating_list.py
@@ -0,0 +1,29 @@
+# what is list?
+'''
+List is a collection of differnt data types and list is used to store multipule values in a single variable.
+list items are ordered, allows duplicates, mutable.
+list item index starts from 0
+list are create using sqaure brackets.
+'''
+# Creating list 
+x = ["python", 100, 90.9] # square brackets are used to create a list 
+print(x)
+print(type(x))
+
+# list constructor
+a = list(("python", 100, 90.9))
+print(a)
+print(type(a))
+
+#List items can be of any data type:
+x = ["python", 100, 90.9, True]
+
+#len() function is used to get the length of the list 
+x = ["python", 100, 90.9]
+print(len(x))
+
+
+
+          
+          
+    
