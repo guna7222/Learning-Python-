e8ae495c59543b6098cda426bf3bc1e1362660b5
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-03-31 15:36:47 +0530

    Create logical_operators.py

---
+++ operators/logical_operators.py
@@ -0,0 +1,7 @@
+# Logical Operators 
+'''
+1)and - Returns True if both statements are True
+2)or - Return True if any one statement is True
+3)not - Return True, if statement is False, it opposite's the answer.
+'''
+# and 
