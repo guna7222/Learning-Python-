5c0bf25cb138bba522b793c9071bb4849a2fe1f5
guna7222 <rrgunasekhar@gmail.com>
2021-04-01 18:49:27 +0530

    updated loop list and list comprehension

---
+++ lists/loop_list.py
@@ -0,0 +1,12 @@
+# loop a list
+fruits = ["apple", "banana", "orange"]
+for i in fruits:
+    print(i)
+    
+# to check whether a specified sequence item is present or not in a list using 'in membership operator'
+fruits = ["apple", "banana", "orange"]
+for i in fruits:
+    if "orange" in i:
+        print("Yes, it is in fruits")
+    else:
+        print("no, it is not avaliable in fruits")   
