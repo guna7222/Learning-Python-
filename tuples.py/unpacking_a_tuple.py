77507ecdc8958ea01a862e2cf4abc84918a36815
guna7222 <rrgunasekhar@gmail.com>
2021-04-02 20:19:54 +0530

    unpacking a tuple

---
+++ tuples.py/unpacking_a_tuple.py
@@ -0,0 +1,17 @@
+# Unpacking a tuple 
+# What is packing?
+'''
+when you create a tuple obviously you will assign a value to it, so this is called packing
+'''
+# What is unpacking?
+'''
+When you have a sequence of collection in a list, tuple or etc.,
+python allows you to extract the value into variables.
+'''
+#Example for unpacking
+
+x = ('tuple', 'list', 'dict')
+a, b, c = x #xtract the value into variables.
+print(a)
+print(b)
+print(c)
