f480c08eb6239fcb7bbf1d7e38ca09bfe0272814
guna7222 <rrgunasekhar@gmail.com>
2021-04-02 07:16:13 +0530

    counting characters in a string

---
+++ strings/count_characters_in_a_string.py
@@ -0,0 +1,4 @@
+# Count characters in a string 
+user_input = input('enter a sequence of character\'s:- ')
+count = len(user_input) # len function is used to count the length of the string.
+print(count)
