15db57f49028b3d0e76b457308f119cd3484c415
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-03 17:29:47 +0530

    Create basic_dictionaries.py

---
+++ dictionaries/basic_dictionaries.py
@@ -0,0 +1,2 @@
+# A dictionary is a collection, which is ordered, indexed, and won't allow duplicate values
+# Note: As of Python 3.7 version dictionaries are ordered. In python below 3.6 are earlier or un-ordered. 
