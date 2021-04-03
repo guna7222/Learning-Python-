cc55640f9501c251ec14fe7b7cb6144a121f7c91
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-03 16:43:07 +0530

    Create remove_set_items.py

---
+++ sets.py/remove_set_items.py
@@ -0,0 +1,19 @@
+# Removing set items 
+# Note: remove method takes exactly only one argument.
+fruits  = {'apple', 'orange', 'watermelon', 'guvva'}
+fruits.remove('orange') # remove method removes the specified value from the set
+print(fruits)
+# remove method throws an error if the specifed value doesn't exits.
+
+# Discard method won't throw an error if the specifed value doesn't exits. 
+fruits  = {'apple', 'orange', 'watermelon', 'guvva'}
+fruits.discard('apple')
+print(fruits)
+
+'''
+pop() method removes last value, but we don't know which value will be removed 
+beacuse sets are unordered.
+'''
+fruits  = {'apple', 'orange', 'watermelon', 'guvva'}
+fruits.pop()
+print(fruits)
