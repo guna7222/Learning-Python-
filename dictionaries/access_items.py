88b4872250ca8fc36e6d4a240fed59cca99a85dd
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-04 16:17:58 +0530

    Update access_items.py

--- dictionaries/access_items.py
+++ dictionaries/access_items.py
@@ -7,20 +7,33 @@ this_dict = {"name":"gunasekhar R", 'age':23, 'door_no': 20.3, 'boo':True}
 access = this_dict["age"]
 print(access)
 
-# get is the another method of accessing items in the dict.
+# get() is the another method of accessing items in the dict.
 this_dict = {"name":"gunasekhar R", 'age':23, 'door_no': 20.3, 'boo':True}
 access = this_dict.get('boo')
 print(access)
 
-# keys method returns a list of all the keys in the dict. 
+# keys() method returns a list of all the keys in the dict. 
 this_dict = {"name":"gunasekhar R", 'age':23, 'door_no': 20.3, 'boo':True}
 key = this_dict.keys()
 print(key)
 this_dict["mobile_no"] = 82923
 print(this_dict)
 
-# values method return a list of all the values in the dict.
+# values() method return a list of all the values in the dict.
 this_dict = {"name":"gunasekhar R", 'age':23, 'door_no': 20.3, 'boo':True}
 val = this_dict.values()
 print(val)
+
+#Items() method will return each item in a dict, as a tuple in a list
+this_dict = {"name":"gunasekhar R", 'age':23, 'door_no': 20.3, 'boo':True}
+item = this_dict.items()
+print(item)
+
+# Checking certain key is present in a dict or not 
+this_dict = {"name":"gunasekhar R", 'age':23, 'door_no': 20.3, 'boo':True}
+if 'name' in this_dict:
+  print('yes avaliable')
+
+
+
 
