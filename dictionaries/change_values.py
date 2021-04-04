a167209cf5997ba0f054b49a3ab7b52054dfa576
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-04 17:01:42 +0530

    Update change_values.py

--- dictionaries/change_values.py
+++ dictionaries/change_values.py
@@ -1,1 +1,19 @@
 # Change a values in a dict.
+this_dict = {
+  "name":"gunasekhar R",
+  'age':23, 
+  'door_no': 20.3, 
+  'boo':True
+}
+this_dict['age'] = 22 # changed age 23 to 22
+print(this_dict)
+
+# the update() method will update the dictionary with the items from the given argument.
+this_dict = {
+  "name":"gunasekhar R",
+  'age':23, 
+  'door_no': 20.3, 
+  'boo':True
+}
+this_dict.update({'friend':'thrilok'})
+print(this_dict)
