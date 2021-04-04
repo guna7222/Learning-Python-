bc50eb129cd8fc5ea8e9c62f7a42ac8dde5fe361
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-04 19:09:35 +0530

    Update remove_items.py

--- dictionaries/remove_items.py
+++ dictionaries/remove_items.py
@@ -1,1 +1,10 @@
 # Removing items 
+# The pop() method will remove the item with specified key
+this_dict =  {
+  "name":"gunasekhar", 
+  "age":22,
+  "address":"vrk",
+  "b.e.cse":75
+}
+this_dict.pop('age')
+print(this_dict)
