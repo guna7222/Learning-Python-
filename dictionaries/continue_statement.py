547a616e0b6337b17c770ac4d2c599ba2c7ab436
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-04 16:38:27 +0530

    Update continue_statement.py

--- dictionaries/continue_statement.py
+++ dictionaries/continue_statement.py
@@ -2,7 +2,7 @@
 this_dict = {"name":"gunasekhar R", 'age':23, 'door_no': 20.3, 'boo':True}
 for i in this_dict:
     if i == 'name':
-        continue 
+        continue # with the continue statement we can stop the current iteration, and continue with the next
     print(i)
     
     
