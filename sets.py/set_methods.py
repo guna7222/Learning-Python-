083825c2e4061701d3002da4f1cac7791919a865
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-04 15:19:55 +0530

    Update set_methods.py

--- sets.py/set_methods.py
+++ sets.py/set_methods.py
@@ -1,1 +1,32 @@
 # set methods
+'''
+add
+update
+del keyword
+pop
+clear
+discard
+remove
+copy
+union
+intersection
+intersection_update
+symentric_difference_update()
+symentric_difference()
+difference
+
+'''
+# add() method is used to insert a value into set.
+fruits = {"orange", "watermelon", "mango"}
+fruits.add('mixed-fruit')
+print(fruits)
+
+# update() method is used to append the set2 values into set1.
+fruits = {"orange", "watermelon", "mango"}
+vegetables = {'carrot', 'tamato', 'onion', 'drumstick'}
+fruits.update(vegetables)
+print(fruits)
+
+#del keyword is used to delete the set
+fruits = {"orange", "watermelon", "mango"}
+del fruits
