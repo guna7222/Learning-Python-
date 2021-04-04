ed66f44f2b8c26846815902022885ebbd14d1e50
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-04 14:47:28 +0530

    Update and rename reverse string to reverse_string.py

---
+++ strings/reverse_string.py
@@ -0,0 +1,7 @@
+# Reverse a string using functions
+def my_function(x): #def is the keyword used to define a function. 
+  return x[::-1]
+
+mytxt = my_function("I wonder how this text looks like backwards")
+
+print(mytxt)
