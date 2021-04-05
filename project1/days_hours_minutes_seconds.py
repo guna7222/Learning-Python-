c151f83901c3b49e9e1c0ff01ed2f1e08f034d39
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-05 17:33:33 +0530

    Update days_hours_minutes_seconds.py

--- project1/days_hours_minutes_seconds.py
+++ project1/days_hours_minutes_seconds.py
@@ -5,3 +5,35 @@ seconds = minutes*60 # formula for converting days = seconds
 print(f'for {days} days {hours} hours ') # formated string used to combine string and interger. 
 print(f'for {days} days {minutes} minutes')
 print(f'for {days} days {seconds} seconds')
+
+
+# new version  
+days = int(input("enter no of days:- ")) # Taking input from user for number of days 
+
+avaliable_menu = input(" \n enter 'h' for hours or enter 'm' for minutes or enter 's' for seconds:- ")
+
+def hours():
+    print(days * 24)
+    
+def minutes():
+    print(days*24*60)
+    
+def seconds():
+    print(days*24*60*60)
+
+while avaliable_menu != 'q':
+    if avaliable_menu == 'h':
+        hours()
+    
+    elif avaliable_menu == 'm':
+        minutes()
+    
+    elif avaliable_menu == 's':
+        seconds()
+    
+    else:
+        print("unknow command")
+        
+    avaliable_menu = input(" enter 'h' for hours or enter 'm' for minutes or enter 's' for seconds:- ")   
+
+
