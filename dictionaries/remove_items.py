6bf799590c965c5593c7d52ba7a18a78432b23e2
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-05 15:57:26 +0530

    Update remove_items.py

--- dictionaries/remove_items.py
+++ dictionaries/remove_items.py
@@ -8,3 +8,32 @@ this_dict =  {
 }
 this_dict.pop('age')
 print(this_dict)
+
+# The popitem() method will remove last inserted item in Python 3.7 version, before 3.6 or earlier version it will remove random item 
+this_dict =  {
+  "name":"gunasekhar", 
+  "age":22,
+  "address":"vrk",
+  "b.e.cse":75
+}
+this_dict.popitem()
+print(this_dict)
+
+# The clear method clears the entire dict. 
+this_dict =  {
+  "name":"gunasekhar", 
+  "age":22,
+  "address":"vrk",
+  "b.e.cse":75
+}
+this_dict.clear()
+print(this_dict)
+
+# The Del keyword deletes the entire dict.
+this_dict =  {
+  "name":"gunasekhar", 
+  "age":22,
+  "address":"vrk",
+  "b.e.cse":75
+}
+del this_dict
