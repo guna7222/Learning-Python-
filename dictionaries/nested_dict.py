7ba05c95a876768e854ae59b99fd23a76046a4c5
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-05 17:00:13 +0530

    Update nested_dict.py

--- dictionaries/nested_dict.py
+++ dictionaries/nested_dict.py
@@ -1,1 +1,35 @@
+# Example 
+this_dict =  {
+  "child1":{
+      "name":"prabhas",
+      "age": 22
+      },
+      "child2":{
+          "name":'hello',
+          "age":1998
+          }
+      }
+  
+print(this_dict)
 
+# copying the dictionary
+    
+this_dict =  {
+  "name":"gunasekhar",
+  "age":22,
+  }
+another_dict = {
+    "name":"thrilok",
+    "age": 23
+}
+  
+last_dict = {
+    "name":"last",
+    "age":22
+}
+final_combine = {
+    "this_dict":this_dict,
+    "another_dict":another_dict,
+    "last_dict":last_dict
+}
+print(final_combine)
