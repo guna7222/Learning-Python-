5bfbdefbb35b74a10a6cb457473299ea5cb64c12
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-06 15:43:40 +0530

    Update basic_if_else.py

--- if_else/basic_if_else.py
+++ if_else/basic_if_else.py
@@ -21,3 +21,28 @@ if a>10:
     print("yes")
 else:
     print("no")
+# short hand if statement
+a = 10
+b = 20
+if a<20: print("yes 10 less than 20")
+    
+#short hand if else statement 
+# short hand if statement
+a = 10
+b = 20
+print("yes") if a<b else print("no")
+
+
+# and logical operator( returns True if both statements are True)
+a = 10
+b = 20
+c = 30
+if a<b and c>b:
+    print("both statements are True")
+    
+# or logical operator return True if any one statement is True.
+a = 10
+b = 20
+c = 30
+if a<b and a>b:
+    print("only one statement is True")
