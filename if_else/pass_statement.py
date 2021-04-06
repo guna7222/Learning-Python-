b408122b9b5af0db887b9b5a5d66dfb45618c9d4
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-06 15:59:06 +0530

    Create pass_statement.py

---
+++ if_else/pass_statement.py
@@ -0,0 +1,9 @@
+# pass statement does nothing it is used simply to avoid getting error!
+'''
+if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+'''
+
+a = 10 
+b = 20
+if a<b:
+  pass
