1b1150ffbdf0ad8168daec9f54b8345b6c8cf0b7
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-06 15:33:22 +0530

    Update basic_if_else.py

--- if_else/basic_if_else.py
+++ if_else/basic_if_else.py
@@ -1,1 +1,23 @@
 # Basic if elif else statements 
+# if statement basic code
+a = 10
+b = 20
+if a<b:
+    print(f'{a} less than {b}')
+
+# elif statement example
+a = 10
+b = 20
+if a>b:
+    print(f'{a} greater than {b}')
+elif a<b:
+    print(f"{a} less than {b}")
+    
+    
+# else statement 
+a = 10
+b = 20
+if a>10:
+    print("yes")
+else:
+    print("no")
