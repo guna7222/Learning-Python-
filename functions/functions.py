daaf8f4d61b5c67a9a106ef79caeb22d3b87e856
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-12 07:46:27 +0530

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -3,3 +3,15 @@ def function():
   print("This is a first function program ")
   
 function() # calling function using function name 
+
+# example 2
+def function(name):
+    '''
+    def keyword is used to declare a function in,
+    python.
+    '''
+    print("hello my name is:- "+ name)
+    
+function('Gunasekhar R')
+print(function.__doc__)
+
