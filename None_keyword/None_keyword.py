fa9254efd3c0fc538fbfd5b9e696ac921f7849be
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-18 08:33:44 +0530

    Create None_keyword.py

---
+++ None_keyword/None_keyword.py
@@ -0,0 +1,21 @@
+'''
+None
+
+None keyword is used to define a null values are no values at all. None keyword is not same as 0, empty_string or False. None means none nothing.
+
+Example
+
+'''
+
+x = None
+if x:
+  print('is it True')
+elif x is False:
+  print('is it False')
+
+else:
+  print('None means, None nothing')
+
+# output :- None means, None nothing
+
+
