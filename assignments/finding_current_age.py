2ccd0739564a3e6cc1c4f60739d6e35efbc605ca
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-18 10:46:12 +0530

    Update finding_current_age.py

--- assignments/finding_current_age.py
+++ assignments/finding_current_age.py
@@ -7,3 +7,15 @@ age = present_year - birth_year
 print(age)
 
 
+# same thing doing with functions
+
+import datetime
+
+def current_age(age):
+  dynamic_year = datetime.datetime.now().year
+  age = dynamic_year - age
+  print(age)
+
+
+age = int(input('enter your current age:- '))
+current_age(age)
