fceb92eb4fbfcbf84a97bc43a048ad3d147179c2
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-21 15:55:37 +0530

    Create dict_values_in_list.py

---
+++ lists/dict_values_in_list.py
@@ -0,0 +1,20 @@
+# using dict values in list.
+
+my_list = [
+    {
+        'name':'gunasekhar',
+        'age':23
+    },
+    {
+        'name':'Thrilok',
+        'age':23
+    }]
+    
+print(my_list[0]['name'])
+
+'''
+
+Note:- In python we will call it as dictionaries, in other programming language
+it might be Hash-Table, Mapping or objects.
+
+'''
