43226b7f2842a684d6abdb063107a97f9933020a
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-04-24 07:53:55 +0530

    Create dict_keys.py

---
+++ dictionaries/dict_keys.py
@@ -0,0 +1,36 @@
+'''
+Dictionary keys must be immutable like(str, int, bool, or tuple).
+
+Dictionaries keys examples:- 
+
+'''
+string = {
+    'str':'value1',
+    'str2':'value2'
+    }
+    
+print(string)
+
+integer = {
+    23:'age',
+    1772832:'salary'
+}
+
+print(integer)
+
+boolean = {
+    True : 'salary hike',
+    False: 'unprecedented'
+}
+print(boolean)
+
+tuple1 = {
+    (1, 'python') : 'example for tuple immutable',
+    (2, 'mysql'):'good'
+}
+
+print(tuple1)
+
+
+
+
