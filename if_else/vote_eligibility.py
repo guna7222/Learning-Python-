8d96aab3ff4ed0dae01278d1e7c35064493ef670
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-02 16:05:22 +0530

    Create vote_eligibility.py

---
+++ if_else/vote_eligibility.py
@@ -0,0 +1,6 @@
+# checking eligible for vote or not 
+age = int(input("enter your age:- "))
+if age >=18:
+    print("you are eligible for vote:- congrats! ")
+else:
+    print("you are not eligible for vote:- pls waite for some years")
