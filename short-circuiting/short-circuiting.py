35dcc54756e2aa7c8bf7bfea903edf7396497411
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-03 14:28:11 +0530

    Update short-circuiting.py

--- short-circuiting/short-circuiting.py
+++ short-circuiting/short-circuiting.py
@@ -3,7 +3,9 @@
 and
 or
 '''
+is_best_friend =  False
 is_friend = True
-is_best_friend =  True
-if is_friend and is_best_friend:
+ if is_best_friend and is_friend: # here is_best_friend is False so it will short-circuit it won't check is_friend because is_best_friend is false.
   print("yes condition is True")
+else:
+  print('condition is False')
