1b839b2c3218a7092778d0e1a62ba57472ab25e3
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-03 11:23:43 +0530

    Update ternary_operator.py

--- if_else/ternary_operator.py
+++ if_else/ternary_operator.py
@@ -5,3 +5,11 @@ can_message = 'can send message' if is_friend else 'not allowed to send msg '
 print(can_message)
 
 # syntax:- condition if condition else condition. 
+
+# example 2
+
+a = 10
+b = 2
+print('yes a is greater than b' if a>b else 'false')
+
+
