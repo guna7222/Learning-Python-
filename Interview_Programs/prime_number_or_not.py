e1f7e93f53d4885a3d2c1541dcaebb257ac8cb06
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-10 08:47:58 +0530

    Update prime_number_or_not.py

--- Interview_Programs/prime_number_or_not.py
+++ Interview_Programs/prime_number_or_not.py
@@ -1,11 +1,12 @@
-# a number that is divisible only by itself Example:-(2,3,5,7) 
-user_input = int(input("enter a number:- "))
+user_input = int(input('enter a number:- '))
 if user_input > 1:
     for i in range(2, user_input):
         if (user_input % i)==0:
-            print("it is not a prime number! ")
-        else:
-            print(f"{user_input} is a prime number! ")
-            
+            print('it is not a prime number')
+            break
+        
+    else:
+        print('it is a prime number')
+        
 else:
-    print("it is not a prime number !")
+    print('enter a valid number')
