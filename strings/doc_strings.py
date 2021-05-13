6521f19be905ee47f9c6c1f6aaed7c4fdafa8b94
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-13 09:03:32 +0530

    Create doc_strings.py

---
+++ strings/doc_strings.py
@@ -0,0 +1,14 @@
+'''
+docstrings are not actually comments but they are documentation strings. Docstrings are within the triple quotes. 
+They are not assigned to any variable therefore it servers as a documentation strings.
+
+'''
+
+# Example 
+'''
+
+example for documentation strings called (Docstrings)
+
+'''
+
+print('above is the example for documentation strings')
