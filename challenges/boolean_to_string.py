7f2a52783643ea9674b36e2c00bcb7e416a5805e
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-20 11:38:59 +0530

    Create boolean_to_string.py

---
+++ challenges/boolean_to_string.py
@@ -0,0 +1,21 @@
+'''
+Boolean to String Conversion
+Create a function that takes a boolean variable flag and returns it as a string.
+
+Examples
+bool_to_string(True) ➞ "True"
+
+bool_to_string(False) ➞ "False"
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+'''
+# solution
+
+def bool_to_string(flag):
+	return str(flag)
+	
+print(bool_to_string(True))
+	
