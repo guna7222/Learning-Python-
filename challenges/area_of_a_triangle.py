1a3b9261e3cd6d4fa29bc5960b629b211455019d
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-20 10:32:54 +0530

    Create area_of_a_triangle.py

---
+++ challenges/area_of_a_triangle.py
@@ -0,0 +1,26 @@
+'''
+Area of a Triangle
+Write a function that takes the base and height of a triangle and return its area.
+
+Examples
+tri_area(3, 2) ➞ 3
+
+tri_area(7, 4) ➞ 14
+
+tri_area(10, 10) ➞ 50
+Notes
+The area of a triangle is: (base * height) / 2
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+'''
+# solution
+
+def tri_area(base, height):
+	area = (base * height) /2 # area of the triangle formula (base * height) / 2
+	return area
+	
+	
+print(tri_area(3, 2))
+	
