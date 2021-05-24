51eea16b4e861dc52eab31f4ab62f6fda371e9bf
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-24 16:42:49 +0530

    Create python_version.py

---
+++ Python_Version/python_version.py
@@ -0,0 +1,12 @@
+# write a code to display current version of python
+
+import sys # sys is the module.
+
+def version():
+    print('Python version: ')
+    print(sys.version)
+    print('version information: ')
+    print(sys.version_info)
+
+version()
+
