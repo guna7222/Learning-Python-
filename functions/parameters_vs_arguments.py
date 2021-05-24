bfbf7ba0caa6e800c2643ec770c10779096cd88a
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-24 14:20:12 +0530

    Create parameters_vs_arguments.py

---
+++ functions/parameters_vs_arguments.py
@@ -0,0 +1,9 @@
+# Hint:- def keyword is used to define a function.
+
+def greetings(name, position):  # name, position is called parameters
+    print(f'Hello, hai welcome Mr. {name} for the role of {position} at python-world')
+
+
+greetings('R.Gunasekhar', 'Associate Software Developer')
+
+# values are called arguments like R.Gunasekhar', 'Associate Software Developer
