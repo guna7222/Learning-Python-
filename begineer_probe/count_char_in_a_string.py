db4271bee28e134df3e15ce04a6589949139f285
guna7222 <rrgunasekhar@gmail.com>
2021-05-25 16:00:02 +0530

    created count characters in a string

---
+++ begineer_probe/count_char_in_a_string.py
@@ -0,0 +1,10 @@
+'''
+Count characters in a string?
+
+'''
+
+def count_characters():
+    string = input('enter a string:- ')
+    print(len(string)) # Here len() function is used to count the characters in a string.
+    
+count_characters()
