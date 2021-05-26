b4a0e3bc048555b10ab4473f39b3f904b54f59cf
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-05-26 20:02:13 +0530

    Update password_generator.py

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -1,1 +1,14 @@
-print('hello')
+# password generator 
+
+
+import random
+
+lower_case = 'abcdefghijklmnopqrstuvwxyz'
+upper_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+symbols = '*#$%&()_@.;'
+
+combine_all = lower_case + upper_case + numbers + symbols
+length = 12
+password = ''.join(random.sample(combine_all, length)) # sample method syntax random.sample(sequence, k) k Required. The size of the returned list
+print(password)
