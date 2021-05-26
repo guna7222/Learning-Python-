562c4896064b01bf6fd86e361cc613cf9ddcf1bc
guna7222 <rrgunasekhar@gmail.com>
2021-05-26 09:50:25 +0530

    : Multi-dimensonal list need to add
    Task-Url:

---
+++ begineer_probe/alphabet_lower_upper.py
@@ -0,0 +1,13 @@
+'''
+ Check if given alphabet is in lower/upper case?
+ 
+ '''
+ 
+alphabet = input('enter a sequence of characters:- ')
+if alphabet.islower():
+    print('It is lower')
+else:
+    print('It is upper')
+    
+# same using function's
+
