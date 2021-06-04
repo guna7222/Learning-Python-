5d8cf2ccb7daa7a046fe024dc7ed4f4ff070b4f2
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-06-04 15:25:36 +0530

    Create return_keyword.py

---
+++ functions/return_keyword.py
@@ -0,0 +1,15 @@
+# return keyword
+'''
+
+The Python return statement is a special statement that you can use
+inside a function or method to send the function's result back to the
+caller.
+
+'''
+
+def sum(num1, num2):
+    return num1 + num2 
+    
+    
+total = sum(12, 12)
+print(sum(12, total))
