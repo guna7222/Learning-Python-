9e3a368093efa554fbd346fc4bb5aa51d6fcf39a
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-06-27 13:47:54 +0530

    Update positive_number_or_negative_number.py

--- Interview_Programs/positive_number_or_negative_number.py
+++ Interview_Programs/positive_number_or_negative_number.py
@@ -6,3 +6,18 @@ elif num == 0:
     print("0 is a positive number")
 else:
     print(f'{num} number is positive number')
+    
+    
+    
+    
+# positive or negative number using functions
+
+def positive_negative(num):
+    if num < 0:
+        print(f'{num} is negative number')
+    else:
+        print(f'{num} is positive number')
+        
+        
+num = int(input('enter a number:- '))
+positive_negative(num)
