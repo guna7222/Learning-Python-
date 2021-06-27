112ec52c784306e8e070e8fc2e57921dd1264a1f
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-06-27 13:20:44 +0530

    Update prime_number_or_not.py

--- Interview_Programs/prime_number_or_not.py
+++ Interview_Programs/prime_number_or_not.py
@@ -10,3 +10,21 @@ if user_input > 1:
         
 else:
     print('enter a valid number')
+    
+    
+    
+# Another way of approach
+
+num = int(input('enter a number:- '))
+if num == 1:
+    print('prime number of 1 is 1')
+elif num < 1:
+    print('enter number greater than 1')
+else:
+    for i in range(2, num):
+        if num % i ==0 :
+            print('not a prime number')
+            break
+    else:
+        print('prime number')
+            
