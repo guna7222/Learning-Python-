41bd1259905d2c450b76cff0cf1e32db99941294
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-06-27 13:54:12 +0530

    Update palindrome_or_not.py

--- Interview_Programs/palindrome_or_not.py
+++ Interview_Programs/palindrome_or_not.py
@@ -5,3 +5,15 @@ if name == name[::-1]: # used to reverse a string
     print("palindrome")
 else:
     print("not a palindrome")
+    
+    
+# palindrome using functions
+
+def palindrome(string):
+    if string == string[::-1]:
+        print(f' {string} palindrome')
+    else:
+        print(f' {string} not a palindrome')
+        
+string = input('enter a string:- ')
+palindrome(string)
