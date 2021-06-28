ecb9a026eb8a732d1f94376d87cf017fdf4b9f3f
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-06-28 08:47:18 +0530

    Update factorial_of_a_number.py

--- Interview_Programs/factorial_of_a_number.py
+++ Interview_Programs/factorial_of_a_number.py
@@ -8,3 +8,22 @@ else:
     for i in range(1, fnum+1):
         factorial = factorial * i
     print(factorial)
+    
+    
+    
+    
+# factorial of a number using return 
+def factorial(number):
+    fact = 1
+   
+    if number <= 0:
+        return 'enter positive number'
+    elif number == 1:
+        return 'Factorial of 1 is 1'
+    else:
+        for i in range(1, number+1):
+            fact = fact*i
+        return fact
+            
+number = int(input('enter a number:- '))
+print(factorial(number))
