d33d16eb40dcfbb9c4c75f1446ee5264dacf0801
Guna Sekhar <47294637+guna7222@users.noreply.github.com>
2021-07-13 15:40:59 +0530

    Create DocStrings.py

---
+++ comments/DocStrings.py
@@ -0,0 +1,16 @@
+
+'''
+Doc strings are not actually comments, but they are documentation strings. Doc strings  
+are with in triple quatates. They are not assigned to any variable so it servers as a multiline comments.
+
+'''
+
+def adding_two_numbers(a, b):
+    '''
+    info:- This function adds two numbers, they numbers that are passed from the parameter.
+    '''
+    print(a+b)
+    
+    
+adding_two_numbers(12, 90)
+    
